c4e84a97a757ca78bdca3e8e328608872af4559c
Peter Stakoun <peter@stakoun.com>
2017-05-22 10:03:29 -0400

    Add habit creation

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,9 +1,23 @@
 import React, { Component, PropTypes } from 'react';
+import ReactDOM from 'react-dom';
 import { createContainer } from 'meteor/react-meteor-data';
 import { Habits } from '../api/habits.js';
 import Habit from './Habit.jsx';
  
 class App extends Component {
+	handleSubmit(event) {
+		event.preventDefault();
+
+		const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+
+		Habits.insert({
+			text,
+			createdAt: new Date(),
+		});
+
+		ReactDOM.findDOMNode(this.refs.textInput).value = '';
+	}
+
  	renderHabits() {
 		return this.props.habits.map((habit) => (
 			<Habit key={habit._id} habit={habit} />
@@ -15,6 +29,9 @@ class App extends Component {
 			<div className="container">
 				<header>
 					<h1>Habits</h1>
+					<form className="new-habit" onSubmit={this.handleSubmit.bind(this)}>
+						<input type="text" ref="textInput" placeholder="Add new habit" />
+					</form>
 				</header>
  				<ul>
 					{this.renderHabits()}
