1c7777d127350125d5198a1e9b8c6d7b7ea48764
Peter Stakoun <peter@stakoun.com>
2017-06-09 12:23:57 -0400

    Add inputs

--- imports/ui/Habit.jsx
+++ imports/ui/Habit.jsx
@@ -1,15 +1,37 @@
 import React, { Component, PropTypes } from 'react';
- 
+import { Habits } from '../api/habits.js';
+
 export default class Habit extends Component {
-  render() {
-    return (
-      <li>{this.props.habit.text}</li>
-    );
-  }
+	toggleChecked() {
+		Habits.update(this.props.habit._id, {
+			$set: { checked: !this.props.habit.checked },
+		});
+	}
+
+	deleteHabit() {
+		Habits.remove(this.props.habit._id);
+	}
+
+	render() {
+		const habitClassName = this.props.habit.checked ? 'checked' : '';
+
+		return (
+			<li className={habitClassName}>
+				<button className="delete" onClick={this.deleteHabit.bind(this)}>
+					&times;
+				</button>
+
+				<input type="checkbox" readOnly
+					checked={this.props.habit.checked}
+					onClick={this.toggleChecked.bind(this)}
+				/>
+
+				<span className="text">{this.props.habit.text}</span>
+			</li>
+		);
+	}
 }
  
 Habit.propTypes = {
-	// This component gets the task to display through a React prop.
-	// We can use propTypes to indicate it is required
 	habit: PropTypes.object.isRequired,
 };
