70e352e90dcd947d00a247d19f733ce455533a17
Peter Stakoun <peter@stakoun.com>
2017-06-19 22:51:46 -0400

    Update graphs

--- imports/ui/Habit.jsx
+++ imports/ui/Habit.jsx
@@ -24,6 +24,24 @@ export default class Habit extends Component {
 	render() {
 		const completed = this.props.habit.datesCompleted.includes(GetDate());
 		const habitClassName = completed ? 'completed' : '';
+		var chartData = {
+			labels: [],
+			datasets: [{
+				data: []
+			}]
+		};
+
+		for (var i = 7; i > 0; i--) {
+			chartData.labels.push('');
+			var count = 0;
+			for (var j = 0; j < this.props.habit.datesCompleted.length; j++) {
+				console.log();
+				if (Math.floor(new Date() - new Date(this.props.habit.datesCompleted[j].substring(0, 4)+'-'+this.props.habit.datesCompleted[j].substring(4, 6)+'-'+this.props.habit.datesCompleted[j].substring(6,8))) / (1000* 60 * 60 * 24) > i) {
+					count++;
+				}
+			}
+			chartData.datasets[0].data.push(count);
+		}
 
 		return (
 			<div className="row">
@@ -41,7 +59,7 @@ export default class Habit extends Component {
 						<span className="text">{this.props.habit.text}</span>
 					</div>
 					<div className="col-sm-6">
-						<LineChart data={null} options={null} />
+						<LineChart data={chartData} height="100" width="345" />
 					</div>
 				</li>
 			</div>
